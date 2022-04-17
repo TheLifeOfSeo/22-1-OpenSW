47fa96d172cd4307e92cb0587d6fb8d7a338ebed
TheLifeOfSeo <dsa3182@gmail.com>
2022-04-17 21:33:03 +0900

    small change

--- HW/HW7/zoom.py
+++ HW/HW7/zoom.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter.filedialog import *
 
 def zoomIn(event) :
     global photo
@@ -9,6 +10,13 @@ def zoomOut(event) :
     global photo
     photo = photo.subsample(2, 2)
     labelImage.configure(image = photo)
+
+def openGif() :
+    fileName = askopenfilename(parent = window, filetypes = (("GIF파일", "*.gif"), ("모든 파일", "*.*")))
+    global photo
+    photo = tk.PhotoImage(file = "fileName")
+    labelImage = tk.Label(window, image = photo)
+    labelImage.pack()
 
 
 
@@ -22,13 +30,21 @@ window.title("확대와 축소")
 window.bind("<Up>", zoomIn)
 window.bind("<Down>", zoomOut)
 
-
-
-
+mainMenu = tk.Menu(window)
+window.config(menu = mainMenu)
+fileMenu = tk.Menu(mainMenu)
+mainMenu.add_cascade(label = "파일", menu = fileMenu)
+fileMenu.add_command(label = "파일 열기", command = openGif)
+fileMenu.add_separator()
 
 photo = tk.PhotoImage(file = "HW/HW7/img/london.gif")
 labelImage = tk.Label(window, image = photo)
 labelImage.pack()
+
+
+
+
+
 
 
 
